efddfa6f9f19af8224a0e40c4a20c4930578c15a
Dick greyson <flowiii@protonmail.com>
2024-07-17 00:26:41 -0400

    Javascript top exercises

---
+++ javascript.js
@@ -0,0 +1,54 @@
+
+//Exercise 1. Add 2 numbers together!
+
+/*let num1 = 10;
+let num2 = 10;
+
+console.log(num1 + num2);
+*/
+
+
+//Exercise 2.Add a sequence of 6 different numbers together.
+/*console.log(1+2+1+2+1+2);*/
+
+//Exercise3. Print the value of the following expression: (4 + 6 + 9) / 77
+/*console.log((4 + 6 + 9) / 77)*/
+
+//Exercise 4.Type this statement at the top of the script tag: let a = 10
+ /*let a = 10;
+ //console.log(a);
+ //console.log(9 * a);
+let b = 7 * a;
+console.log(b);*/
+
+//Exercise 5. Declare a constant variable MAX with the value 57
+//Set another variable actual to MAX - 13
+//Set another variable percentage to actual / MAX
+//If you type percentage in the console and press Enter you should see a value like 0.7719
+
+/*let MAX = 57;
+let actual = MAX - 13;
+let percentage = actual /MAX;
+
+console.log(percentage);
+*/
+
+
+/*let firstName = "manny";
+let lastName = "erazo";
+let thisYear = 2024;
+let birthYear = 1991;
+
+
+const greeting = "Hello my name is " + firstName + " " + "and i am " + (thisYear - birthYear) + " years old.";
+
+console.log(greeting);
+
+*/
+
+
+let name1 = "manny";
+
+name1 = name1.toUpperCase();
+
+console.log(name1);
